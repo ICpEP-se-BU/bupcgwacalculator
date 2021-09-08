a2a8ef4f0a9628d316773fafbd2d2a15a04967d6
Daniel A. Soriano II <72549944+EngrJill@users.noreply.github.com>
2021-09-08 19:49:31 +0800

    Placed warning message on main page

---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Main-container">
+      <img src="@/assets/maintenance.jpg" alt="BUPC GWA Calculator on Maintenance">
+      <h1>Go to Courses, still working on the Main Landing Page where you still have to input the units and grades</h1>
+      <router-link to="/courses"  >Go to Courses</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,14 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/',
+    name: 'Main',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../components/Main.vue')
+  },
+  {
     path: '/courses',
     name: 'Home',
     component: Home
